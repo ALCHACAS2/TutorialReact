958727e5f1a0d9be74b6a8dca2a5c3f878f5704a
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-03-20 23:10:07 -0600

    change main

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import './App.css'
+function App(){
+  return( 
+    <div className='App'>
+    </div>
+  )
+}
+export default App
