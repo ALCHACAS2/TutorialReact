938f624fbca59e6ff867a37e04ce555aa452ce56
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-03-21 14:38:37 -0600

    [EDIT vite,userForm] peque;os cambios
    Agregue for a los label, userForm
    Se formateo el texto, vite

--- my-app/src/component/userForm.jsx
+++ my-app/src/component/userForm.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 function UserForm(props) {
-const [name, setName] = useState("");
-const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     props.onSubmit({ name, email });
     setName("");
     setEmail("");
-};
-return (
+  };
+  return (
     <form onSubmit={handleSubmit}>
-    <label>
+      <label for="name">
         Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-    </label>
-    <label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} id="name" />
+      </label>
+      <label for="email">
         Email:
-        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-    </label>
-    <button type="submit">Add User</button>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} id="email" />
+      </label>
+      <button type="submit">Add User</button>
     </form>
-);
+  );
 }
 export default UserForm;
